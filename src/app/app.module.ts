2533276b069aa0367f7a485676edc7d6ac3e65ff
Neomaster <the@neomaster.net>
2022-09-15 15:25:10 +0300

    intro1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Intro1Component } from './intro1/intro1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Intro1Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/intro1/intro1.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-intro1',
+  templateUrl: './intro1.component.html',
+  styleUrls: ['./intro1.component.css']
+})
+export class Intro1Component implements OnInit {
+
+  obs?: Observable<string>;
+  sub?: Subscription;
+
+  constructor() {
+    this.obs = new Observable(function subscribe(subscriber) {
+      let i = 1;
+      setInterval(() => subscriber.next(`msg ${i++}`), 500);
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  subscribe(): void {
+    if (!this.sub) {
+      console.log("subscribed");
+      this.sub = this.obs?.subscribe(msg => console.log(msg));
+    }
+  }
+
+  unsubscribe(): void {
+    if (this.sub) {
+      console.log("unsubscribed");
+      this.sub.unsubscribe();
+      this.sub = undefined;
+    }
+  }
+}
