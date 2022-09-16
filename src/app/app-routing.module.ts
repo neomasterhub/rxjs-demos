ba2f12e9574eff1c179bc5e511dfdd42120bc9fb
Neomaster <the@neomaster.net>
2022-09-16 22:51:56 +0300

    Dynamic router links.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { Intro1Component } from './intro1/intro1.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {Intro1Component} from './intro1/intro1.component';
 
 const routes: Routes = [
-  { path: 'intro-1', component: Intro1Component }
+  {path: 'intro-1', component: Intro1Component, title: '1. Подписка и отписка'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  static get routeLinks() {
+    return routes.map(r => {
+      return {
+        path: r.path,
+        title: r.title
+      }
+    });
+  }
+}
